28e471d462f1deaf0750ea69cb30a2d3a0829db6
Krayavarapu <krayavarapu198@gmail.com>
2020-09-23 16:55:44 -0400

    added routes to routing module

--- course-project-a4-hot-off-the-press/src/app/app-routing.module.ts
+++ course-project-a4-hot-off-the-press/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: AppComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
