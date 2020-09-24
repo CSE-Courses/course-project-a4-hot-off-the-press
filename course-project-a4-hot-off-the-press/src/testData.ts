fdab26a3bdc95387c19926569a390a8bbf41f3c3
Ben Badaszewski <badaszewskib@gmail.com>
2020-09-23 21:01:38 -0400

    #13 Have added a horizontal scroll to albumList to fix clipping on small displays

--- course-project-a4-hot-off-the-press/src/testData.ts
+++ course-project-a4-hot-off-the-press/src/testData.ts
@@ -1,7 +1,7 @@
 import { Album } from './app/models/album';
 
 export const NEWRELEASES: Album[] = [
-    { title: "Songlines", artist: "Derek Trucks Band", image: "/assets/songlines.jpg" },
+    { title: "Songlines", artist: "Derek Trucks Bandaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", image: "/assets/songlines.jpg" },
     { title: "Hill Climber", artist: "Vulfpeck", image: "/assets/hillclimber.jpg" },
     { title: "Choose Your Weapon", artist: "Hiatus Kaiyote", image: "/assets/cyw.jpg" },
     { title: "Santana I", artist: "Carlos Santana", image: "/assets/santana1.png" },
