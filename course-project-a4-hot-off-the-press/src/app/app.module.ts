4605ef2a4df169d591185a2f8fe0885a3d880266
Krayavarapu <krayavarapu198@gmail.com>
2020-09-26 16:44:38 -0400

    a few changes to login component

--- course-project-a4-hot-off-the-press/src/app/app.module.ts
+++ course-project-a4-hot-off-the-press/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AdminComponent
+    AdminComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
