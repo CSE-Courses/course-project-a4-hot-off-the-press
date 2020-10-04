63c5a83627acd0e9d07ab159690215f9bc249fdc
TintinYi <42983961+TintinYi@users.noreply.github.com>
2020-10-04 03:32:40 -0400

    #17 Shopping Cart Template

---
+++ course-project-a4-hot-off-the-press/src/shopping cart/js/carts.js
@@ -0,0 +1,239 @@
+
+$(function () {
+
+    //Global checkbox selected and unselected styles
+    var $allCheckbox = $('input[type="checkbox"]'),     //All checkboxes in the world
+        $wholeChexbox = $('.whole_check'),
+        $cartBox = $('.cartBox'),                       //Every store box
+        $shopCheckbox = $('.shopChoice'),               //Checkbox for each shop
+        $sonCheckBox = $('.son_check');                 //Checkbox of goods under each store
+    $allCheckbox.click(function () {
+        if ($(this).is(':checked')) {
+            $(this).next('label').addClass('mark');
+        } else {
+            $(this).next('label').removeClass('mark')
+        }
+    });
+
+    //===============================================The relationship between global selection and a single product================================
+    $wholeChexbox.click(function () {
+        var $checkboxs = $cartBox.find('input[type="checkbox"]');
+        if ($(this).is(':checked')) {
+            $checkboxs.prop("checked", true);
+            $checkboxs.next('label').addClass('mark');
+        } else {
+            $checkboxs.prop("checked", false);
+            $checkboxs.next('label').removeClass('mark');
+        }
+        totalMoney();
+    });
+
+
+    $sonCheckBox.each(function () {
+        $(this).click(function () {
+            if ($(this).is(':checked')) {
+                //Judgment: Whether all individual products are checked
+                var len = $sonCheckBox.length;
+                var num = 0;
+                $sonCheckBox.each(function () {
+                    if ($(this).is(':checked')) {
+                        num++;
+                    }
+                });
+                if (num == len) {
+                    $wholeChexbox.prop("checked", true);
+                    $wholeChexbox.next('label').addClass('mark');
+                }
+            } else {
+                //Single item unchecked, global select unchecked
+                $wholeChexbox.prop("checked", false);
+                $wholeChexbox.next('label').removeClass('mark');
+            }
+        })
+    })
+
+    //=======================================The relationship between the checkbox of each store and the select all checkbox/the change of each store and its product style===================================================
+
+    //If one store is not selected, the global select all button is unchecked. If all stores are selected, the global select all button is checked.
+    $shopCheckbox.each(function () {
+        $(this).click(function () {
+            if ($(this).is(':checked')) {
+                //Judgment: If all shops are selected, the global select all button is ticked.
+                var len = $shopCheckbox.length;
+                var num = 0;
+                $shopCheckbox.each(function () {
+                    if ($(this).is(':checked')) {
+                        num++;
+                    }
+                });
+                if (num == len) {
+                    $wholeChexbox.prop("checked", true);
+                    $wholeChexbox.next('label').addClass('mark');
+                }
+
+                //Checkbox selected state under the store
+                $(this).parents('.cartBox').find('.son_check').prop("checked", true);
+                $(this).parents('.cartBox').find('.son_check').next('label').addClass('mark');
+            } else {
+                //Otherwise, uncheck the global select button
+                $wholeChexbox.prop("checked", false);
+                $wholeChexbox.next('label').removeClass('mark');
+
+                //Checkbox selected state under the store
+                $(this).parents('.cartBox').find('.son_check').prop("checked", false);
+                $(this).parents('.cartBox').find('.son_check').next('label').removeClass('mark');
+            }
+            totalMoney();
+        });
+    });
+
+
+    //========================================The relationship between the checkbox of each store and the checkbox of its products======================================================
+
+    //One of the shop $sonChecks is unchecked, and the shop select all button is unchecked. If all shops are checked, select all and tick
+    $cartBox.each(function () {
+        var $this = $(this);
+        var $sonChecks = $this.find('.son_check');
+        $sonChecks.each(function () {
+            $(this).click(function () {
+                if ($(this).is(':checked')) {
+                    //Judgment: If all $sonChecks are selected, then all shops are selected and tick!
+                    var len = $sonChecks.length;
+                    var num = 0;
+                    $sonChecks.each(function () {
+                        if ($(this).is(':checked')) {
+                            num++;
+                        }
+                    });
+                    if (num == len) {
+                        $(this).parents('.cartBox').find('.shopChoice').prop("checked", true);
+                        $(this).parents('.cartBox').find('.shopChoice').next('label').addClass('mark');
+                    }
+
+                } else {
+                    //Otherwise, select all shops and cancel
+                    $(this).parents('.cartBox').find('.shopChoice').prop("checked", false);
+                    $(this).parents('.cartBox').find('.shopChoice').next('label').removeClass('mark');
+                }
+                totalMoney();
+            });
+        });
+    });
+
+
+    //=================================================Number of Products==============================================
+    var $plus = $('.plus'),
+        $reduce = $('.reduce'),
+        $all_sum = $('.sum');
+    $plus.click(function () {
+        var $inputVal = $(this).prev('input'),
+            $count = parseInt($inputVal.val())+1,
+            $obj = $(this).parents('.amount_box').find('.reduce'),
+            $priceTotalObj = $(this).parents('.order_lists').find('.sum_price'),
+            $price = $(this).parents('.order_lists').find('.price').html(),  //unit price
+            $priceTotal = $count*parseInt($price.substring(1));
+        $inputVal.val($count);
+        $priceTotalObj.html('￥'+$priceTotal);
+        if($inputVal.val()>1 && $obj.hasClass('reSty')){
+            $obj.removeClass('reSty');
+        }
+        totalMoney();
+    });
+
+    $reduce.click(function () {
+        var $inputVal = $(this).next('input'),
+            $count = parseInt($inputVal.val())-1,
+            $priceTotalObj = $(this).parents('.order_lists').find('.sum_price'),
+            $price = $(this).parents('.order_lists').find('.price').html(),  //unit price
+            $priceTotal = $count*parseInt($price.substring(1));
+        if($inputVal.val()>1){
+            $inputVal.val($count);
+            $priceTotalObj.html('￥'+$priceTotal);
+        }
+        if($inputVal.val()==1 && !$(this).hasClass('reSty')){
+            $(this).addClass('reSty');
+        }
+        totalMoney();
+    });
+
+    $all_sum.keyup(function () {
+        var $count = 0,
+            $priceTotalObj = $(this).parents('.order_lists').find('.sum_price'),
+            $price = $(this).parents('.order_lists').find('.price').html(),  //unit price
+            $priceTotal = 0;
+        if($(this).val()==''){
+            $(this).val('1');
+        }
+        $(this).val($(this).val().replace(/\D|^0/g,''));
+        $count = $(this).val();
+        $priceTotal = $count*parseInt($price.substring(1));
+        $(this).attr('value',$count);
+        $priceTotalObj.html('￥'+$priceTotal);
+        totalMoney();
+    })
+
+    //======================================Remove product========================================
+
+    var $order_lists = null;
+    var $order_content = '';
+    $('.delBtn').click(function () {
+        $order_lists = $(this).parents('.order_lists');
+        $order_content = $order_lists.parents('.order_content');
+        $('.model_bg').fadeIn(300);
+        $('.my_model').fadeIn(300);
+    });
+
+    //Close modal
+    $('.closeModel').click(function () {
+        closeM();
+    });
+    $('.dialog-close').click(function () {
+        closeM();
+    });
+    function closeM() {
+        $('.model_bg').fadeOut(300);
+        $('.my_model').fadeOut(300);
+    }
+    //Confirm button, remove product
+    $('.dialog-sure').click(function () {
+        $order_lists.remove();
+        if($order_content.html().trim() == null || $order_content.html().trim().length == 0){
+            $order_content.parents('.cartBox').remove();
+        }
+        closeM();
+        $sonCheckBox = $('.son_check');
+        totalMoney();
+    })
+
+    //======================================total==========================================
+
+    function totalMoney() {
+        var total_money = 0;
+        var total_count = 0;
+        var calBtn = $('.calBtn a');
+        $sonCheckBox.each(function () {
+            if ($(this).is(':checked')) {
+                var goods = parseInt($(this).parents('.order_lists').find('.sum_price').html().substring(1));
+                var num =  parseInt($(this).parents('.order_lists').find('.sum').val());
+                total_money += goods;
+                total_count += num;
+            }
+        });
+        $('.total_text').html('￥'+total_money);
+        $('.piece_num').html(total_count);
+
+        // console.log(total_money,total_count);
+
+        if(total_money!=0 && total_count!=0){
+            if(!calBtn.hasClass('btn_sty')){
+                calBtn.addClass('btn_sty');
+            }
+        }else{
+            if(calBtn.hasClass('btn_sty')){
+                calBtn.removeClass('btn_sty');
+            }
+        }
+    }
+
+
+});
