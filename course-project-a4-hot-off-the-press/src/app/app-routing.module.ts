0316d07fadec972b6522bbd803e77157674c17a4
Krayavarapu <krayavarapu198@gmail.com>
2020-10-07 19:22:24 -0400

    added route to routing-module

--- course-project-a4-hot-off-the-press/src/app/app-routing.module.ts
+++ course-project-a4-hot-off-the-press/src/app/app-routing.module.ts
@@ -7,9 +7,11 @@ import { LoginComponent } from './components/login/login.component'
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { UserCreationComponent } from './components/user-creation/user-creation.component';
 
 const routes: Routes = [
   { path: 'login',component: LoginComponent },
+  { path: 'create-account', component: UserCreationComponent},
   { path: 'settings', component:ProductListComponent },
   { path: '', component: LandingPageComponent },
   // {
---
+++ course-project-a4-hot-off-the-press/src/app/components/user-creation/user-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-creation',
+  templateUrl: './user-creation.component.html',
+  styleUrls: ['./user-creation.component.scss']
+})
+export class UserCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
