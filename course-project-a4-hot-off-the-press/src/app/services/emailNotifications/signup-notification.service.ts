4d4cbd317133c8a663960c53c949511d4ca43667
Ben Badaszewski <badaszewskib@gmail.com>
2020-10-13 18:34:23 -0400

    #45 Created purchase-notification service

---
+++ course-project-a4-hot-off-the-press/src/app/services/emailNotifications/signup-notification.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignupNotificationService{
+  private baseURL:string = "https://97c2xci62k.execute-api.us-east-2.amazonaws.com/alpha/signup/";
+  private uname:string; //Username of new account
+  private dest:string; //Destination email to send to
+  
+  constructor(private http:HttpClient) {
+  }
+
+  public sendNotification(username:string, destEmail:string):Observable<string>{
+    this.dest = destEmail;
+    this.uname = username;
+    return this.http.get<string>(this.baseURL, {
+      params: {
+        destination: this.dest,
+        username: this.uname
+      }
+    })
+  }
+
+}
