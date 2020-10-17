39061683f962ac4428d19b2efd4581d6cbe74863
TintinYi <42983961+TintinYi@users.noreply.github.com>
2020-10-17 18:19:00 -0400

    #17 Angular shopping cart

---
+++ course-project-a4-hot-off-the-press/shopping cart.angular/src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name: "Item 1",
+    price: 799,
+    description: "balabala"
+  },
+  {
+    name: "Item 2",
+    price: 699,
+    description: "balabala"
+  },
+  {
+    name: "Item 3",
+    price: 299,
+    description: "balabala"
+  }
+];
--- course-project-a4-hot-off-the-press/src/app/models/album.ts
+++
@@ -1,5 +0,0 @@
-export class Album {
-    artist:string;
-    title:string;
-    image:string;
-}
