139f7403329d0117edc17b6569ca7bcb2f21d7dd
TintinYi <42983961+TintinYi@users.noreply.github.com>
2020-10-18 00:28:23 -0400

    #49 Notify me when sale

---
+++ Notify me/src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name: "Item 1",
+    price: 200,
+    description: "In Stock"
+  },
+  {
+    name: "Item 2",
+    price: 201,
+    description: "In Stock"
+  },
+  {
+    name: "Item 3",
+    price: 200,
+    description: "In Stock"
+  }
+];
